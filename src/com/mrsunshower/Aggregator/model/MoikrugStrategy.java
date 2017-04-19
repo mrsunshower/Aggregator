f7caae28b5bc63557fe8b028d23b1acfc869daa9
mrsunshower <who.is.hindenburg@gmail.com>
2017-04-19 12:37:06 +0300

    MoikrugStrategy refactoring

--- src/com/mrsunshower/Aggregator/model/MoikrugStrategy.java
+++ src/com/mrsunshower/Aggregator/model/MoikrugStrategy.java
@@ -15,65 +15,48 @@ import java.util.List;
  */
 
 public class MoikrugStrategy implements Strategy {
-    private static final String URL_FORMAT = "https://moikrug.ru/vacancies?page=%d&q=java+%s";
+    private static final String URL_FORMAT = "https://moikrug.ru/vacancies?q=java+%s&page=%d";
     //private static final String URL_FORMAT = "http://93.190.137.51/testdata/big28data%d%s.html";
 
 
     @Override
-    public List<Vacancy> getVacancies(String searchString) {
-        List<Vacancy> vacancies = new ArrayList<>();
-        int page = 1;
-        Document document;
-        Elements elements;
-        try {
-            while((document = getDocument(searchString, page++)) != null && !(elements = document.getElementsByClass("job")).isEmpty()) {
-                for (Element element : elements) {
-                    String siteName = "https://moikrug.ru";
-
-                    String title = "";
-                    String url = "";
-                    Element titleElement = element.getElementsByClass("title").first();
-                    if (titleElement != null) {
-                        title = titleElement.getElementsByTag("a").first().text();
-                        url = siteName + titleElement.getElementsByTag("a").first().attr("href");
-                    }
-
-                    String companyName = "";
-                    Element companyNameElement = element.getElementsByClass("company_name").first();
-                    if(companyNameElement != null) {
-                        companyName = companyNameElement.getElementsByTag("a").first().text();
-                    }
-
-                    String city = "";
-                    Element cityElement = element.getElementsByClass("location").first();
-                    if (cityElement != null) {
-                        city = cityElement.getElementsByTag("a").first().text();
-                    }
-
-                    String salary = "";
-                    Element salaryElement = element.getElementsByClass("count").first();
-                    if (salaryElement != null) {
-                        salary = salaryElement.text();
-                    }
-
-                    Vacancy vacancy = new Vacancy();
-                    vacancy.setTitle(title);
-                    vacancy.setUrl(url);
-                    vacancy.setCity(city);
-                    vacancy.setCompanyName(companyName);
-                    vacancy.setSiteName(siteName);
-                    vacancy.setSalary(salary);
-
-                    vacancies.add(vacancy);
+    public List<Vacancy> getVacancies(String searchString)
+    {
+        List<Vacancy> Vacancies = new ArrayList<>();
+        int pageNum = 0;
+        Document doc = null;
+        while(true)
+        {
+            try {
+                doc = getDocument(searchString, pageNum);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Elements vacancies = doc.getElementsByClass("job");
+            if (vacancies.size()==0) break;
+            for (Element element: vacancies)
+            {
+                if (element != null)
+                {
+                    Vacancy vac = new Vacancy();
+                    vac.setTitle(element.getElementsByAttributeValue("class", "title").text());
+                    vac.setCompanyName(element.getElementsByAttributeValue("class", "company_name").text());
+                    vac.setSiteName(URL_FORMAT);
+                    vac.setUrl("https://moikrug.ru" + element.select("a[class=job_icon]").attr("href"));
+                    String salary = element.getElementsByAttributeValue("class", "salary").text();
+                    String city = element.getElementsByAttributeValue("class", "location").text();
+                    vac.setSalary(salary.length()==0 ? "" : salary);
+                    vac.setCity(city.length()==0 ? "" : city);
+                    Vacancies.add(vac);
                 }
             }
-        } catch (IOException e) {}
-
-        return vacancies;
+            pageNum++;
+        }
+        return Vacancies;
     }
 
     protected Document getDocument(String searchString, int page) throws IOException {
-        String url = String.format(URL_FORMAT, page, searchString);
+        String url = String.format(URL_FORMAT, searchString, page);
         Document document = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.1144").referrer("none").get();
 
         return document;
